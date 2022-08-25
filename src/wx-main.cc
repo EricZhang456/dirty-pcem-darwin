4399d50f6fd019b29db8c1f837f35cbee68025a0
Eric Zhang <ericxiang456@gmail.com>
2022-08-25 13:36:52 +0800

    init

---
+++ src/wx-main.cc
@@ -0,0 +1,20 @@
+#include "wx-app.h"
+//#include <SDL2/SDL.h>
+#include "/opt/local/include/SDL2/SDL.h"
+
+
+extern "C"
+{
+int pc_main(int, char**);
+}
+
+int main(int argc, char **argv)
+{
+        if (!pc_main(argc, argv))
+                return -1;
+
+        wxApp::SetInstance(new App());
+        wxEntry(argc, argv);
+        return 0;
+}
+
